1d0abb2df25737ad446adf70de6f8bcbe2032afd
Leighton Chen <leighton@asia.com>
2019-03-09 10:34:12 -0800

    Remove morgan for production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
 
 const router = require('./router.js');
@@ -9,7 +9,7 @@ const app = express();
 const port = 3333;
 
 app.use(cors());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 app.use(express.static(`${__dirname}/../public`));
 app.use('/:id', express.static(`${__dirname}/../public`));
