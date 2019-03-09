875468b628c4369049faec29c0b291b3888a31e2
Leighton Chen <leighton@asia.com>
2019-03-08 16:03:00 -0800

    Refactor to use body-parser only when necessary

--- server/router.js
+++ server/router.js
@@ -1,10 +1,13 @@
+const parser = require('body-parser');
 const router = require('express').Router();
 const controller = require('./controller.js');
+
+const bp = parser.json();
 
 router.get('/load/:restaurantId', controller.load.get);
 router.get('/query/:restaurantId/:date/:time', controller.bookings.get);
 router.post('/book/:restaurantId/:date/:time', controller.bookings.post);
-router.put('/update/:id', controller.bookings.put);
-router.delete('/delete/:id', controller.bookings.delete);
+router.put('/update/:id', bp, controller.bookings.put);
+router.delete('/delete/:id', bp, controller.bookings.delete);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('newrelic');
 const express = require('express');
 // const morgan = require('morgan');
-const parser = require('body-parser');
 const cors = require('cors');
 
 const router = require('./router.js');
@@ -11,9 +10,8 @@ const port = 3333;
 
 app.use(cors());
 // app.use(morgan('dev'));
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
 
+app.use(express.static(`${__dirname}/../public`));
 app.use('/:id', express.static(`${__dirname}/../public`));
 
 app.use('/api/reserve', router);
